e42fc074b93654382772c522ac8bcddb23b55e69
Hoàng Đại <daihoangnam3it@gmail.com>
2022-01-02 21:25:45 +0700

    🚩 practical with useState and useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from "react"
+import Navbar from "./components/Navbar";
+import './styles/App.css'
 export function App() {
     return (
        <div>
+          <Navbar/>
           <h1>Hello World!</h1>
        </div>
     );
---
+++ src/components/WelcomeMessage.tsx
@@ -0,0 +1,15 @@
+import { Box } from '@material-ui/core'
+import React from 'react'
+interface WelcomeMessageProps{
+  position:string;
+  country?:string
+}
+const WelcomeMessage = ({position,country='Some where'}:WelcomeMessageProps) => {
+  return (
+    <Box mb={1}>
+      {position} {country}
+    </Box>
+  )
+}
+
+export default WelcomeMessage
